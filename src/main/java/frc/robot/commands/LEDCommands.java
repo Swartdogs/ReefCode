36222b9d5df6a356bf33e16e0f5fada7405c375f
Cedric <25cedliu@student.cfschools.org>
2025-01-30 18:32:31 -0600

    Made Flash Color Better

--- src/main/java/frc/robot/commands/LEDCommands.java
+++ src/main/java/frc/robot/commands/LEDCommands.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.leds.LED;
 
 public class LEDCommands
@@ -18,11 +19,11 @@ public class LEDCommands
 
     public static Command flashColor(LED led, Color color)
     {
-        return led.run(() ->
+        return led.runOnce(() ->
         {
             led.setFlashing(true);
             led.setColor(color);
-        }).finallyDo(() -> led.setFlashing(false));
+        }).andThen(Commands.idle(led)).finallyDo(() -> led.setFlashing(false));
     }
 
     public static Command setDefaultColor(LED led, Color color)
