0354d92edb85d0693321af602c15e0dec36ed659
Cedric <25cedliu@student.cfschools.org>
2025-03-25 20:58:56 -0500

    Update at 'Tue Mar 25 20:58:56 CDT 2025'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -149,8 +149,8 @@ public final class Constants
 
     public static class Elevator
     {
-        public static final double  RAW_SENSOR_MIN               = 0.628;
-        public static final double  RAW_SENSOR_MAX               = 0.3877;
+        public static final double  RAW_SENSOR_MIN               = 0.625;
+        public static final double  RAW_SENSOR_MAX               = 0.3829;
         public static final double  MIN_EXTENSION                = 14.75;
         public static final double  MAX_EXTENSION                = 77.0;
         public static final double  EXTENSION_KP                 = 0.5;
